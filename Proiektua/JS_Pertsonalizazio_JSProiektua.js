f067620b678de3e380cd8ba7b8ff09d34ae9b742
Unai875 <usolaexpos1@educacion.navarra.es>
2025-02-21 19:40:40 +0100

    Add files via upload

--- Proiektua/JS_Pertsonalizazio_JSProiektua.js
+++ Proiektua/JS_Pertsonalizazio_JSProiektua.js
@@ -1,3 +1,48 @@
 function arroxa(){
-    document.body.style.backgroundColor = "pink"
+    document.body.style.backgroundColor = "pink";
+    document.getElementById("taula").style.backgroundColor = "white";
+}
+
+function lima(){
+    document.body.style.backgroundColor = "lightgreen";
+    document.getElementById("taula").style.backgroundColor = "white";
+}
+
+function zerua(){
+    document.body.style.backgroundColor = "lightblue";
+    document.getElementById("taula").style.backgroundColor = "white";
+}
+
+function gris_iluna(){
+    document.body.style.backgroundColor = "#444";
+    document.getElementById("taula").style.backgroundColor = "white";
+}
+
+function gris_argia(){
+    document.body.style.backgroundColor = "lightgrey";
+    document.getElementById("taula").style.backgroundColor = "white";
+}
+
+function gorria(){
+    document.getElementById("Paragrafoa").style.color = "red";
+}
+
+function berdea(){
+    document.getElementById("Paragrafoa").style.color = "green";
+}
+
+function urdina(){
+    document.getElementById("Paragrafoa").style.color = "blue";
+}
+
+function beltza(){
+    document.getElementById("Paragrafoa").style.color = "black";
+}
+
+function txuria(){
+    document.getElementById("Paragrafoa").style.color = "white";
+}
+
+function xs(){
+    document.getElementById("Paragrafoa").style.fontSize = "x-small";
 }
