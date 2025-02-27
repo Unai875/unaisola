2e869297ac6620b9a47b24efab17e5c544cc666c
Unai875 <usolaexpos1@educacion.navarra.es>
2025-02-27 16:33:29 +0100

    Add files via upload

--- Proiektua/JS_Formularioa_JSProiektua.js
+++ Proiektua/JS_Formularioa_JSProiektua.js
@@ -14,3 +14,24 @@ function konprobaketa(event){
 }
 
 document.getElementById("konprobaketa").addEventListener("click", konprobaketa);
+
+function modua(){
+
+    const modua = document.getElementsByClassName("m_botoia")[0].value;
+
+    if(document.getElementsByClassName("m_botoia")[0].includes("Argia") || document.getElementsByClassName("m_botoia")[0].includes("Modua")){
+        document.getElementsByClassName("m_botoia")[0].textContent = "Iluna";
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        document.getElementsByClassName("formulario-kontainerra")[0].style.backgroundColor = "#444";
+        document.getElementsByClassName("formulario-kontainerra")[0].style.borderBottomLeftRadius = "5px";
+        document.getElementsByClassName("formulario-kontainerra")[0].style.borderBottomRightRadius = "5px";
+    } else {
+        document.getElementsByClassName("m_botoia")[0].textContent = "Argia";
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.getElementsByClassName("formulario-kontainerra")[0].style.backgroundColor = "white";
+        document.getElementsByClassName("formulario-kontainerra")[0].style.borderBottomLeftRadius = "5px";
+        document.getElementsByClassName("formulario-kontainerra")[0].style.borderBottomRightRadius = "5px";
+    }
+}
